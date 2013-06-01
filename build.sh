25a45817a67d5ac5d32ff55869b076be145f6470
jenkins@master <jenkins@cyanogenmod.com>
2013-06-01 20:44:43 +0200

    This repo doesn't exist anymore

--- build.sh
+++ build.sh
@@ -131,12 +131,6 @@ fi
 mkdir -p .repo/local_manifests
 rm -f .repo/local_manifest.xml
 
-rm -rf $WORKSPACE/build_env
-git clone https://github.com/CyanogenMod/cm_build_config.git $WORKSPACE/build_env
-check_result "Bootstrap failed"
-
-cp $WORKSPACE/build_env/$REPO_BRANCH.xml .repo/local_manifests/dyn-$REPO_BRANCH.xml
-
 echo Core Manifest:
 cat .repo/manifest.xml
 
